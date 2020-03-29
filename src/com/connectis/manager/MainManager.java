226015d371d1b5592f37b00a1ea9c67a5ae1ec8a
Piotr Śrótwa <altar10420@gmail.com>
2020-03-29 22:49:12 +0200

    Add README.md

--- src/com/connectis/manager/MainManager.java
+++ src/com/connectis/manager/MainManager.java
@@ -1,24 +1,6 @@
 package com.connectis.manager;
 
 /*
-Napisz program, który posłuży Ci jako menadżer haseł.
-Program powinien mieć następujące funkcjonalności:
-
-- Po podaniu adresu strony oraz nazwy użytkownika program zwraca zapisane dla danego loginu hasło
-- Jeśli dla takiej domeny/loginu program nie posiada hasła, użytkownik otrzymuje odpowiedni komunikat
-- Program potrafi na żądanie wygenerować nowe hasło (losowy ciąg znaków alfanumerycznych,
- można zaimplementować samemu lub użyć jakiegoś gotowego rozwiązania z sieci)
-- Użytkownik ma możliwość dodania nowego hasła dla wybranej pary domena/login
-    - Program nie przyjmuje duplikatów. Jeśli wpis istnieje, powinien poprosić użytkownika
-     o to czy chce nadpisać hasło.
-- Program potrafi wyświetlić pełny raport, posortowany po adresach domen,
- a następnie po loginach. Raport wyświetla Adres domeny, Login oraz zapamiętane hasło
-- Wszystkie dane trzymamy w pamięci w odpowiednich strukturach danych (tj. bez bazy danych)
-    - Dla chętnych: dane trzymane w pliku, podczas startu aplikacji,
-     jeśli taki plik istnieje, program wczytuje zapisane dane z tego pliku (persystencja)
- */
-
-/*
     private static String wczytajZnaki() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         return reader.readLine();
